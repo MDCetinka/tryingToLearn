1f1021c028bef4d7fcf8b484f30043b6d2a51ffe
MDCetinka <melissa.dchetinkaya@gmail.com>
2024-12-02 19:17:14 +0300

    is this even working

--- TrialOneGit.java
+++ TrialOneGit.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class TrialOneGit {
+    public static void main(String[] args) {
+        System.out.println("Sus? among us.");
+
+
+        System.out.println("Why are you tormenting me");
+
+    }
+
 }
